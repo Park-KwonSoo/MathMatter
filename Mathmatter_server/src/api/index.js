20f20ce3970e5d51d5d4da87e0d8c4ecda656f5d
Park Kwon Soo <redbean096@naver.com>
2020-12-13 18:40:58 +0900

    add Write Router

--- Mathmatter_server/src/api/index.js
+++ Mathmatter_server/src/api/index.js
@@ -4,9 +4,11 @@ const api = new Router();
 const print = require("./print");
 const profile = require("./profile");
 const auth = require("./auth");
+const write = require("./write");
 
 api.use("/print", print.routes());
 api.use("/profile", profile.routes());
 api.use("/auth", auth.routes());
+api.use("/write", write.routes());
 
 module.exports = api;
