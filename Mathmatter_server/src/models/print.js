d879c1bb3b89aff3f440bf1c94a965a95ebf8d52
Park-KwonSoo <redbean096@naver.com>
2021-02-13 21:54:38 +0900

    remark change

--- Mathmatter_server/src/models/print.js
+++ Mathmatter_server/src/models/print.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const PrintSchema = new Schema ({
     //내신형 , 수능형 , 유형별 타입을 선택함
     typeOfPrint : { type : String, required : true },
-    //중간고사인지, 기말고사인지, 모의고사인지, 시험 대비가 아닌지	 등을 저장
+    //중간고사(1)인지, 기말고사(2)인지, 모의고사(3)인지, 시험 대비(0)가 아닌지	 등을 저장
     typeOfExam : Number,
     //문제 개수를 저장
     numberOfQuestion : Number,
