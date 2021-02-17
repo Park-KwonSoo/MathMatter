96c64e4691d1538616867e14e54889d106b9b8f7
Park-KwonSoo <redbean096@naver.com>
2021-02-17 19:35:45 +0900

    edit axios func

--- mathmatter-client/src/lib/api/write.js
+++ mathmatter-client/src/lib/api/write.js
@@ -4,5 +4,5 @@ export const writing = ({title, body}) => client.post('/api/write/write', {title
 export const seeBoard = () => client.get('/api/write/all');
 export const seeMyWritingList = () => client.get('/api/profile/getWriteList');
 export const seeWriting = (postId) => client.get('/api/write/' + postId);
-export const replying = (postId) => client.post('/api/write/' + postId);
+export const replying = (postId) => client.patch('/api/write/' + postId);
 export const deleteWriting = (postId) => client.delete('/api/write/' + postId);
