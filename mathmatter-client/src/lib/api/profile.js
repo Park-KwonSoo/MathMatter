10aefe7262672751f41001907659266b84c93a2d
Park-KwonSoo <redbean096@naver.com>
2021-02-18 04:12:12 +0900

    axios edit

--- mathmatter-client/src/lib/api/profile.js
+++ mathmatter-client/src/lib/api/profile.js
@@ -1,5 +1,4 @@
 import client from './client';
 
 export const getProfile = () => client.get('/api/profile/getProfile');
-
 export const patchProfile = ({birth, phoneNumber, userName}) => client.patch('/api/profile/setProfile', {birth, phoneNumber, userName});
